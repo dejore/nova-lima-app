f332b444e8863cff4f8b3a4c47ef054ce1b5e058
dejore <dejore@dejore.com.br>
2018-02-27 21:32:21 -0300

    App Ejetado

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import React from "react";
-import { AppRegistry } from "react-native";
-import App from "./App";
-AppRegistry.registerComponent("Nova Lima", () => App);
+import { AppRegistry } from 'react-native';
+import App from './App';
+AppRegistry.registerComponent('novalimaapp', () => App);
